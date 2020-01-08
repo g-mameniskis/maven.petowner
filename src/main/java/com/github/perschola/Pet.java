1078de22c5d87969aa7b08cc613e80010dc06c24
unknown <g.mameniskis1@gmail.com>
2020-01-08 09:32:14 -0500

    pet completed

--- src/main/java/com/github/perschola/Pet.java
+++ src/main/java/com/github/perschola/Pet.java
@@ -4,17 +4,25 @@ package com.github.perschola;
  * Created by leon on 12/17/2019.
  */
 public class Pet {
+
+    private String name;
+    private int age;
+
     public Pet() {
+        this.name = "";
+        this.age = -1;
     }
 
     public Pet(String name, Integer age) {
+        this.name = name;
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
     }
 }
